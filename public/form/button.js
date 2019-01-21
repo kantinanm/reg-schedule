762554dbbd9b3a125c8abc77f2bb17baa4b4932b
matzeeya <matzeeya@gmail.com>
2019-01-21 16:38:21 +0700

    button css

---
+++ public/form/button.js
@@ -0,0 +1,16 @@
+(function() {
+    var removeSuccess;
+  
+    removeSuccess = function() {
+      return $('.button').removeClass('success');
+    };
+  
+    $(document).ready(function() {
+      return $('.button').click(function() {
+        $(this).addClass('success');
+        return setTimeout(removeSuccess, 3000);
+      });
+    });
+  
+  }).call(this);
+  
